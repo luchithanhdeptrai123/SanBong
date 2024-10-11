dff606b10aa47d6c2caf4793b056d4cf08c86d98
rhanh <tentongan@gmail.com>
2024-10-12 00:37:18 +0700

    Add project files.

---
+++ SanBong/Controllers/TrangChuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SanBong.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
